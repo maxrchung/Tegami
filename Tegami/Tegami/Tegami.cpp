2c9ae0689a57f24f96c4df65f8efbf4fde6b520a
maxrchung <maxrchung@gmail.com>
2017-06-11 14:22:05 -0700

    Fleshed out CPP for better example

--- Tegami/Tegami/Tegami.cpp
+++ Tegami/Tegami/Tegami.cpp
@@ -1,10 +1,11 @@
 #include "Sprite.hpp"
 #include "Storyboard.hpp"
+#include "Time.hpp"
 #include <fstream>
 
 void main() {
-	Sprite* sprite = new Sprite("");
-	sprite->Move(0, 0, Vector2::Zero, Vector2::Zero);
+	Sprite* sprite = new Sprite("image.png", Vector2::Zero, Layer::Foreground, Origin::Centre);
+	sprite->Move(0, Time("00:10:000").ms, Vector2(-100.0f,-100.0f), Vector2(100.0f, 100.0f));
 
 	// Create a file StoryboardInputPath.txt in Tegami\Tegami\Tegami
 	// That's kind of a confusing directory, so to be a bit more clear, make sure the text file is placed on the same level as this Tegami.cpp file
