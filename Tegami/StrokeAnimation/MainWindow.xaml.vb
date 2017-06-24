d642e7648900fdbdb62f9d4262406cede611c023
maxrchung <maxrchung@gmail.com>
2017-06-23 20:08:52 -0700

    Changed FormatTime to include milliseconds; video display/opacity controls; line follows mousedown/mouseup; line display/opacity controls; creating a line adds into Lines canvas; volume controls

--- Tegami/StrokeAnimation/MainWindow.xaml.vb
+++ Tegami/StrokeAnimation/MainWindow.xaml.vb
@@ -3,11 +3,14 @@ Imports System.Windows.Threading
 
 Class MainWindow
     Dim playState As PlayState = PlayState.None
-    Dim timeSpan As TimeSpan
-    Dim dispatcherTimer As DispatcherTimer
+    Dim timeSpanTotal As TimeSpan
+    Dim progressTimer As DispatcherTimer
+    Dim currentPoint As Point
+    Dim lineTimer As DispatcherTimer
+    Dim currentLine As Line
 
-    Private Function FormatTime(minutes As Integer, seconds As Integer)
-        Dim time As String = String.Format("{0}:{1}", minutes.ToString("D2"), seconds.ToString("D2"))
+    Private Function FormatTime(timeSpan As TimeSpan)
+        Dim time As String = String.Format("{0}:{1}:{2}", timeSpan.Minutes.ToString("D2"), timeSpan.Seconds.ToString("D2"), timeSpan.Milliseconds.ToString("D3"))
         Return time
     End Function
 
@@ -49,32 +52,113 @@ Class MainWindow
 
     ' https://stackoverflow.com/questions/10208959/binding-mediaelement-to-slider-position-in-wpf
     Private Sub Player_MediaOpened(sender As Object, e As RoutedEventArgs)
-        timeSpan = Player.NaturalDuration.TimeSpan
+        timeSpanTotal = Player.NaturalDuration.TimeSpan
 
-        dispatcherTimer = New DispatcherTimer()
-        dispatcherTimer.Interval = TimeSpan.FromSeconds(1)
-        AddHandler dispatcherTimer.Tick, AddressOf timer_Tick
-        dispatcherTimer.Start()
+        progressTimer = New DispatcherTimer(DispatcherPriority.Render)
+        progressTimer.Interval = TimeSpan.FromSeconds(0.001)
+        AddHandler progressTimer.Tick, AddressOf progressTimer_Tick
+        progressTimer.Start()
     End Sub
 
-    Private Sub timer_Tick(sender As Object, e As EventArgs)
+    Private Sub progressTimer_Tick(sender As Object, e As EventArgs)
         If Player.NaturalDuration.TimeSpan.TotalSeconds > 0 Then
-            If timeSpan.TotalSeconds > 0 Then
-                Progress.Value = Player.Position.TotalSeconds / timeSpan.TotalSeconds
+            If timeSpanTotal.TotalSeconds > 0 Then
+                Progress.Value = Player.Position.TotalSeconds / timeSpanTotal.TotalSeconds
 
-                CurrentTime.Content = FormatTime(Player.Position.Minutes, Player.Position.Seconds)
-                TotalTime.Content = FormatTime(timeSpan.Minutes, timeSpan.Seconds)
+                CurrentTime.Content = FormatTime(Player.Position)
+                TotalTime.Content = FormatTime(timeSpanTotal)
             End If
         End If
     End Sub
 
-    Private Sub Progress_Load(sender As Object, e As EventArgs)
-        AddHandler Progress.MouseDown, AddressOf Progress_Click
+    Private Sub Progress_Loaded(sender As Object, e As EventArgs)
+        AddHandler Progress.MouseDown, AddressOf Progress_MouseDown
     End Sub
 
-    Private Sub Progress_Click(sender As Object, e As MouseEventArgs)
+    Private Sub Progress_MouseDown(sender As Object, e As MouseEventArgs)
         Dim ratio As Double = e.GetPosition(Progress).X / Progress.ActualWidth
         Progress.Value = ratio * Progress.Maximum
-        Player.Position = New TimeSpan(0, 0, ratio * timeSpan.TotalSeconds)
+        Player.Position = New TimeSpan(0, 0, ratio * timeSpanTotal.TotalSeconds)
+    End Sub
+
+    Private Sub VideoDisplay_Checked(sender As Object, e As EventArgs)
+        Player.Visibility = Visibility.Visible
+        VideoOpacity.IsEnabled = True
+    End Sub
+
+    Private Sub VideoDisplay_Unchecked(sender As Object, e As EventArgs)
+        Player.Visibility = Visibility.Hidden
+        VideoOpacity.IsEnabled = False
+    End Sub
+
+    Private Sub VideoOpacity_ValueChanged(sender As Object, e As EventArgs)
+        Player.Opacity = VideoOpacity.Value
+    End Sub
+
+    Private Sub Panel_Loaded(sender As Object, e As EventArgs)
+        AddHandler Panel.MouseDown, AddressOf Panel_MouseDown
+        AddHandler Panel.MouseUp, AddressOf Panel_MouseUp
+    End Sub
+
+    Private Sub Panel_MouseDown(sender As Object, e As MouseEventArgs)
+        currentPoint = e.GetPosition(Panel)
+        currentLine = ConstructLine(currentPoint, currentPoint)
+        Lines.Children.Add(currentLine)
+
+        lineTimer = New DispatcherTimer(DispatcherPriority.Render)
+        lineTimer.Interval = TimeSpan.FromSeconds(0.001)
+        AddHandler lineTimer.Tick, AddressOf lineTimer_Tick
+        lineTimer.Start()
+    End Sub
+
+    Private Sub Panel_MouseUp(sender As Object, e As MouseEventArgs)
+        Dim endPoint = e.GetPosition(Panel)
+        currentLine = ConstructLine(currentPoint, endPoint)
+    End Sub
+
+    Private Sub lineTimer_Tick(sender As Object, e As EventArgs)
+        Dim point As Point = Mouse.GetPosition(Panel)
+        currentLine.X2 = point.X
+        currentLine.Y2 = point.Y
+    End Sub
+
+    Private Function ConstructLine(first As Point, second As Point) As Line
+        Dim line As New Line()
+        line.X1 = first.X
+        line.Y1 = first.Y
+        line.X2 = second.X
+        line.Y2 = second.Y
+        line.Stroke = Brushes.Black
+        line.Visibility = Lines.Visibility
+        line.Opacity = Lines.Opacity
+        Return line
+    End Function
+
+    Private Sub LinesDisplay_Checked(sender As Object, e As EventArgs)
+        Lines.Visibility = Visibility.Visible
+        LinesOpacity.IsEnabled = True
+    End Sub
+
+    Private Sub LinesDisplay_Unchecked(sender As Object, e As EventArgs)
+        Lines.Visibility = Visibility.Hidden
+        LinesOpacity.IsEnabled = False
+    End Sub
+
+    Private Sub LinesOpacity_ValueChanged(sender As Object, e As EventArgs)
+        Lines.Opacity = LinesOpacity.Value
+    End Sub
+
+    Private Sub Volume_Checked(sender As Object, e As EventArgs)
+        Player.IsMuted = False
+        VolumeLevel.IsEnabled = True
+    End Sub
+
+    Private Sub Volume_Unchecked(sender As Object, e As EventArgs)
+        Player.IsMuted = True
+        VolumeLevel.IsEnabled = False
+    End Sub
+
+    Private Sub VolumeLevel_ValueChanged(sender As Object, e As EventArgs)
+        Player.Volume = VolumeLevel.Value
     End Sub
 End Class
